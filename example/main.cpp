a52c560f45e150825c53cd564c1ffc782fe40fbd
Igor Klepacki <negan@ctemplar.com>
2020-09-15 03:34:38 +0200

    Add parentheses example

--- example/main.cpp
+++ example/main.cpp
@@ -7,18 +7,18 @@
 int main( ) {
   using namespace solver::internal;
 
-  Lexer lexer { "x + 30 - 20 = y" };
+  Lexer lexer { "x + (15 + 15) - 20 = y" };
 
-  std::cout << std::setw(10)
+  std::cout << std::setw(20)
             << "Type" << " | " << "Inner"
             << std::endl
-            << "--------------------"
+            << std::string(40, '-')
             << std::endl;
 
   for ( auto token = lexer.next( );
         !token.is_one_of( Token::Type::Eof, Token::Type::Invalid );
         token = lexer.next( ) ) {
-    std::cout << std::setw( 10 ) << Token::type_str( token.type( ) ) << " | "
+    std::cout << std::setw( 20 ) << Token::type_str( token.type( ) ) << " | "
               << token.inner( ) << std::endl;
   }
 
